64a35b5dae4b43c20956c009f8dbb506726334c8
João Mendonça <152927513+mendoonca@users.noreply.github.com>
2024-01-02 21:42:53 +0000

    Update Trabalho.java

--- src/Trabalho.java
+++ src/Trabalho.java
@@ -1,6 +1,8 @@
+package t2;
 import java.util.Scanner;
 
 public class Trabalho {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -14,48 +16,7 @@ public class Trabalho {
         apresentarMenuPrincipal(scanner, nome, quanto, preco, feito, nItens);
     }
 
-    public static String apresentarEditarLista() {
-        String editarLista = "----------------------------------------\n"
-                + "(I)nserir item no fim da lista.\n"
-                + "Inserir item na (p)osição n da lista.\n"
-                + "(A)pagar último item inserido na lista.\n"
-                + "Apagar item na posição (n) da lista.\n"
-                + "(a)pagar itens da posição m à n da lista.\n"
-                + "(L)istar todos os itens.\n"
-                + "(V)oltar.\n"
-                + "----------------------------------------\n";
-        return editarLista;
-    }
-
-    public static void apresentarFazerCompras() {
-        String fazerCompras = "----------------------------------------\n"
-                + "(M)arcar primeiro item por comprar.\n"
-                + "(D)esmarcar primeiro item comprado.\n"
-                + "Trocar estado por (n)ome.\n"
-                + "Trocar estado por (p)osição.\n"
-                + "(L)istar.\n"
-                + "(V)oltar.\n"
-                + "----------------------------------------\n";
-    }
-
-    public static void apresentarListar() {
-        String listar = "----------------------------------------\n"
-                + "Listar (t)odos os itens.\n"
-                + "Listar itens (c)omprados.\n"
-                + "Listar itens (p)or comprar.\n"
-                + "----------------------------------------\n";
-    }
-
-    public static void apresentarFazerContas() {
-        String fazerContas = "Quanto custa a (l)ista?\n"
-                + "Quanto já (g)astei?\n"
-                + "Quanto custa o que (f)alta comprar?\n"
-                + "Qual o preço (m)édio por item?\n"
-                + "----------------------------------------\n";
-    }
-
-
-    public static void apresentarMenuPrincipal(Scanner scanner, String[] nome, double[] quanto, double[] preco, boolean[] feito, int nItens) {
+    private static void apresentarMenuPrincipal(Scanner scanner, String[] nome, double[] quanto, double[] preco, boolean[] feito, int nItens) {
         char op;
         do {
             System.out.println("(E)ditar lista.\n"
@@ -66,30 +27,38 @@ public class Trabalho {
             scanner.nextLine();
             switch (op) {
                 case 'E':
-                    apresentarEditarLista(scanner, nome, quanto, preco, feito, nItens);
+                	apresentarEditarLista(scanner, nome, quanto, preco, feito, nItens);
+                    break;
+                case 'F':
+                    apresentarFazerCompras(scanner, nome, quanto, preco, feito, nItens);
+                    break;
+                case 'c':
+                    apresentarFazerContas(scanner, nome, quanto, preco, feito, nItens);
                     break;
             }
         } while (op != 'S');
     }
-    private static void apresentarEditarLista(Scanner scanner, String[] nome, double[] quanto, double[] preco, boolean[] feito, int nItens) {
-        System.out.println(apresentarEditarLista());
-        char opEditarLista = scanner.next().charAt(0);
+
+    private static int apresentarEditarLista(Scanner scanner, String[] nome, double[] quanto, double[] preco, boolean[] feito, int nItens) {
+    	char opEditarLista;
+    	System.out.println(apresentarEditarLista());
+        opEditarLista = scanner.next().charAt(0);
         scanner.nextLine();
         switch (opEditarLista) {
             case 'I':
-                inserirItem(scanner, nome, quanto, preco, feito, nItens);
+            	inserirItem(scanner, nome, quanto, preco, feito, nItens);
                 break;
             case 'p':
-                opcaoP(scanner, nome, quanto, preco, feito, nItens);
+                inserirItemNaposicao(scanner, nome, quanto, preco, feito, nItens);
                 break;
-            /*case 'A':
-                (nome, quanto, preco, feito, nItens);
+            case 'A':
+                apagarUltimo(nome, quanto, preco, feito, nItens);
                 break;
             case 'n':
                 apagarItemNaPosicao(scanner, nome, quanto, preco, feito, nItens);
                 break;
             case 'a':
-                apagarItensDaPosicao(scanner, nome, quanto, preco, feito, nItens);
+            	apagarItensDasPosicoes(scanner, nome, quanto, preco, feito, nItens);
                 break;
             case 'L':
                 listarItens(nome, quanto, preco, feito, nItens);
@@ -98,96 +67,207 @@ public class Trabalho {
                 // Voltar
                 break;
             default:
-                System.out.println("Opção Inválida!");*/
+                System.out.println("Opção Inválida!");
+        }
+        return nItens;
+    }
+
+    private static void apresentarFazerCompras(Scanner scanner, String[] nome, double[] quanto, double[] preco, boolean[] feito, int nItens) {
+        // Implementar apresentarFazerCompras e seus casos
+    }
+
+    private static void apresentarFazerContas(Scanner scanner, String[] nome, double[] quanto, double[] preco, boolean[] feito, int nItens) {
+        // Implementar apresentarFazerContas e seus casos
+    }
+
+    private static String apresentarEditarLista() {
+        return "----------------------------------------\n"
+                + "(I)nserir item no fim da lista.\n"
+                + "Inserir item na (p)osição n da lista.\n"
+                + "(A)pagar último item inserido na lista.\n"
+                + "Apagar item na posição (n) da lista.\n"
+                + "(a)pagar itens da posição m à n da lista.\n"
+                + "(L)istar todos os itens.\n"
+                + "(V)oltar.\n"
+                + "----------------------------------------\n";
+    }
+
+    private static int inserirItem(Scanner scanner, String[] nome, double[] quanto, double[] preco, boolean[] feito, int nItens) {
+    	System.out.println("Insira um item novo para a lista:");
+        String novoItem = scanner.next();
+        nome[nItens] = novoItem;
+
+        System.out.println("Insira o preço do item adicionado:");
+        double novoPreco = scanner.nextDouble();
+        preco[nItens] = novoPreco;
+
+        System.out.println("Insira a quantidade do item adicionado:");
+        int quantidade = scanner.nextInt();
+        quanto[nItens] = quantidade;
+
+        boolean comprado = false;
+        feito[nItens] = comprado;
+
+        // Adiciona +1 à variável nItens sempre que um item novo é adicionado
+        nItens++;
+
+        System.out.println( "┏--------------------------------------┒\n"
+                +   "|      Item adicionado com sucesso!    |\n"
+                +   "┗--------------------------------------┛\n");
+        return nItens;
+    }
+
+    private static void inserirItemNaposicao(Scanner scanner, String[] nome, double[] quanto, double[] preco, boolean[] feito, int nItens) {
+    	// Mostra a Lista
+        listarItens(nome, quanto, preco, feito, nItens);
+
+        System.out.println("Insira a posição onde deseja inserir o novo item:");
+        int posicaoEscolhida = scanner.nextInt();
+        scanner.nextLine();
+        
+        // Verifica se a posição inserida é válida
+        if (posicaoEscolhida < 0 || posicaoEscolhida > nItens) {
+            System.out.println("Posição inválida!");
+        } else {
+            // Desloca os itens para para o novo item na posição escolhida
+            for (int i = nItens - 1; i >= posicaoEscolhida; i--) {
+                nome[i + 1] = nome[i];
+                quanto[i + 1] = quanto[i];
+                preco[i + 1] = preco[i];
+                feito[i + 1] = feito[i];
+            }
+
+            // Solicita info sobre um novo item
+            System.out.println("Insira o nome do novo item:");
+            String nomeNovoItem = scanner.nextLine();
+            nome[posicaoEscolhida] = nomeNovoItem;
+
+            System.out.println("Insira o preço do novo item:");
+            double precoNovoItem = scanner.nextDouble();
+            preco[posicaoEscolhida] = precoNovoItem;
+
+            System.out.println("Insira a quantidade do novo item:");
+            double quantidadeNovoItem = scanner.nextDouble();
+            quanto[posicaoEscolhida] = quantidadeNovoItem;
+
+            boolean novoItemComprado = false;
+            feito[posicaoEscolhida] = novoItemComprado;
+
+            nItens++;
+
+            System.out.println("┏--------------------------------------┒\n"
+                    + "|      Item adicionado com sucesso!    |\n"
+                    + "┗--------------------------------------┛\n");
+
+            // Mostra a lista atualizada
+            listarItens(nome, quanto, preco, feito, nItens);
         }
     }
 
-    /////////////METODO////////////
-    //Insere um novo item à lista//
-private static void inserirItem(Scanner scanner, String[] nome, double[] quanto, double[] preco, boolean[] feito, int nItens){
+    private static void apagarUltimo(String[] nome, double[] quanto, double[] preco, boolean[] feito, int nItens) {
+    	if (nItens > 0) {
+            // Reduz o contador de itens
+            nItens--;
 
-    System.out.println("Insira um item novo para a lista:");
-    String novoItem = scanner.nextLine();
-    nome[nItens] = novoItem;
+            // Imprime mensagem informando qual item foi removido
+            System.out.println("Item removido: " + nome[nItens]);
 
-    System.out.println("Insira o preço do item adicionado:");
-    double novoPreco = scanner.nextDouble();
-    preco[nItens] = novoPreco;
-
-    System.out.println("Insira a quantidade do item adicionado:");
-    int quatidade = scanner.nextInt();
-    quanto[nItens] = quatidade;
-
-    boolean comprado = false;
-    feito[nItens] = comprado;
-
-    //Adiciona +1 à variavel nItens sempre que um item novo é adicionado
-    nItens++;
-
-    System.out.println( "┏--------------------------------------┒\n"
-            +   "|      Item adicionado com sucesso!    |\n"
-            +   "┗--------------------------------------┛\n");
-}
-
-
-    ////////////////////METODO////////////////////
-    //Insere um novo item numa posição escolhida//
-private static void opcaoP(Scanner scanner, String[] nome, double[] quanto, double[] preco, boolean[] feito, int nItens) {
-//Mostra a Lista para o usuario ter uma referência
-    System.out.println("Aqui tem a Lista com todos os seus Itens:");
-    System.out.printf("%s   %-25s  %-10s  %-8s  %-8s\n", " ", "Item", "Quantidade", "Preço", "Comprado");
-    for (int i = 0; i < nItens; i++) {
-        char marcado = feito[i] ? 'x' : ' ';
-        System.out.printf("%d:  %-25s  %-10.2f  %-8.2f  %-8c\n", i, nome[i], quanto[i], preco[i], marcado);
+            // Limpa os arrays para o último item removido
+            nome[nItens] = null;
+            quanto[nItens] = 0.0;
+            preco[nItens] = 0.0;
+            feito[nItens] = false;
+        } else {
+            System.out.println("A lista está vazia. Não há itens para remover.");
+        }
     }
 
-    System.out.println("Insira a posição onde deseja inserir o novo item:");
-    int posicaoEscolhida = scanner.nextInt();
-    scanner.nextLine();
+    private static void apagarItemNaPosicao(Scanner scanner, String[] nome, double[] quanto, double[] preco, boolean[] feito, int nItens) {
+    	// Mostra a Lista
+        listarItens(nome, quanto, preco, feito, nItens);
+        
+    	System.out.println("Insira a posição do item que deseja apagar:");
 
-    // Verifica se a posição inserida é válida
-    if (posicaoEscolhida < 0 || posicaoEscolhida > nItens) {
-        System.out.println("Posição inválida!");
+        if (nItens > 0) {
+            System.out.println("Digite a posição do item a ser apagado:");
+            int posicao = scanner.nextInt();
+
+            if (posicao >= 0 && posicao < nItens) {
+                // Remove o item na posição especificada e reorganiza a lista
+                for (int i = posicao; i < nItens - 1; i++) {
+                    nome[i] = nome[i + 1];
+                    quanto[i] = quanto[i + 1];
+                    preco[i] = preco[i + 1];
+                    feito[i] = feito[i + 1];
+                }
+                nItens--;
+                System.out.println( "┏------------------------------------------┒\n"
+                        +   "  Item na posição " + posicao + " removido com sucesso!  \n"
+                        +   "┗------------------------------------------┛\n");
+
+            } else {
+                System.out.println( "┏--------------------------------------┒\n"
+                        +   "|            Posição inválida          |\n"
+                        +   "┗--------------------------------------┛\n");
+            }
+        } else {
+            System.out.println( "┏--------------------------------------┒\n"
+                    +   "|  A lista está vazia! Nada a remover. |\n"
+                    +   "┗--------------------------------------┛\n");
+        }
     }
 
-    // Desloca os itens para abrir espaço para o novo item na posição escolhida
-    for (int i = nItens - 1; i >= posicaoEscolhida; i--) {
-        nome[i+1] = nome[i];
-        quanto[i+1] = quanto[i];
-        preco[i+1] = preco[i];
-        feito[i+1] = feito[i];
+    private static void apagarItensDasPosicoes(Scanner scanner, String[] nome, double[] quanto, double[] preco, boolean[] feito, int nItens) {
+    	// Mostra a Lista
+        listarItens(nome, quanto, preco, feito, nItens);
+
+        if (nItens > 0) {
+            System.out.println("Digite a posição inicial (m) dos itens a serem apagados:");
+            int pInicial = scanner.nextInt();
+            System.out.println("Digite a posição final (n) dos itens a serem apagados:");
+            int pFinal = scanner.nextInt();
+
+            //Verifica se as posições são válidas e calcula as posições que foram removidas
+            if (pInicial >= 0 && pFinal < nItens && pInicial <= pFinal) {
+                int qRemovidos = pFinal - pInicial + 1;
+
+                //Remove os itens da lista e desloca os outros para preencher os espaços vazios
+                for (int i = 0; i < nItens - qRemovidos; i++) {
+                    if (i < pInicial) {
+                        continue;
+                    }
+                    nome[i] = nome[i + qRemovidos];
+                    quanto[i] = quanto[i + qRemovidos];
+                    preco[i] = preco[i + qRemovidos];
+                    feito[i] = feito[i + qRemovidos];
+                }
+                //Atualiza o número de itens
+                nItens -= qRemovidos;
+
+                System.out.println( "┏--------------------------------------------------┒\n"
+                        +   "  Itens da posição " + pInicial + " à posição " +pFinal + " foram removidos.  \n"
+                        +   "┗--------------------------------------------------┛\n");
+
+            } else {
+                System.out.println( "┏--------------------------------------┒\n"
+                        +   "|         Posições inválidas.          |\n"
+                        +   "┗--------------------------------------┛\n");
+            }
+        } else {
+            System.out.println( "┏--------------------------------------┒\n"
+                    +   "|         A lista está vazia.          |\n"
+                    +   "┗--------------------------------------┛\n");
+        }
     }
 
-    //Solicita detalhes sobre um novo item
-    System.out.println("Insira o nome do novo item:");
-    String nomeNovoItem = scanner.nextLine();
-    nome[posicaoEscolhida] = nomeNovoItem;
-
-    System.out.println("Insira o preço do novo item:");
-    double precoNovoItem = scanner.nextDouble();
-    preco[posicaoEscolhida] = precoNovoItem;
-
-    System.out.println("Insira a quantidade do novo item:");
-    double quantidadeNovoItem = scanner.nextDouble();
-    quanto[posicaoEscolhida] = quantidadeNovoItem;
-
-    boolean novoItemComprado = false;
-    feito[posicaoEscolhida] = novoItemComprado;
-
-    nItens++;
-
-    System.out.println( "┏--------------------------------------┒\n"
-            +   "|      Item adicionado com sucesso!    |\n"
-            +   "┗--------------------------------------┛\n");
-
-
-
-    System.out.println("Aqui tem a sua lista atualizada! ");
-    System.out.printf("%s   %-25s  %-10s  %-8s  %-8s\n", " ", "Item", "Quantidade", "Preço", "Comprado");
-    for (int i = 0; i < nItens; i++) {
-        char marcado = feito[i] ? 'x' : ' ';
-        System.out.printf("%d:  %-25s  %-10.2f  %-8.2f  %-8c\n", i, nome[i], quanto[i], preco[i], marcado);
-    }
-    System.out.println("\n---------------------------------------------------------------\n");
+    private static void listarItens(String[] nome, double[] quanto, double[] preco, boolean[] feito, int nItens) {
+    	//Lista com todos os itens
+        System.out.println("Aqui tem a Lista com todos os seus Itens:");
+        System.out.printf("%s   %-25s  %-10s  %-8s  %-8s\n", " ", "Item", "Quantidade", "Preço", "Comprado");
+        for (int i = 0; i < nItens; i++) {
+            char marcado = feito[i] ? 'x' : ' ';
+            System.out.printf("%d:  %-25s  %-10.2f  %-8.2f  %-8c\n", i, nome[i], quanto[i], preco[i], marcado);
+        }
+        System.out.println("\n---------------------------------------------------------------\n");
     }
 }
